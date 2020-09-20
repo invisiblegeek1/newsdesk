765dd4106dda1b37eed770bf2656303404eb3a56
invisiblegeek1 <48353856+invisiblegeek1@users.noreply.github.com>
2020-09-20 09:33:24 +0000

    new commit

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap.css';
 import './App.css';
 import Navbar from "./navbar/navbar";
 import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
--- newsapp/src/navbar/navbar.js
+++ newsapp/src/navbar/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap';
 import "./navbar.css";
 import {withRouter} from 'react-router-dom';
 
@@ -7,55 +7,21 @@ import {withRouter} from 'react-router-dom';
 
 
 function NavBar (){
-  
-  const items = document.querySelector(".nav-items");
-  const form = document.querySelector("form");
-  let menuBtn = ()=>{
-    items.classList.add("active");
-    menuBtn.classList.add("hide");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-  }
-  let cancelBtn = ()=>{
-    items.classList.remove("active");
-    menuBtn.classList.remove("hide");
-    searchBtn.classList.remove("hide");
-    cancelBtn.classList.remove("show");
-    form.classList.remove("active");
-    cancelBtn.style.color = "#ff3d00";
-  }
-  let searchBtn= ()=>{
-    form.classList.add("active");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-  }
   return (
-
-    
-    <div>
-    <nav>
-      <div className="menu-icon">
-<span className="fas fa-bars" onClick={menuBtn}></span></div>
-<div className="logo">
-NewsDesk</div>
-<div className="nav-items">
-<li><a href="/">Home</a></li>
-<li><a href="/Business">Business</a></li>
-<li><a href="/science">science</a></li>
-<li><a href="/health">health</a></li>
-<li><a href="/sports">sports</a></li>
-<li><a href="/entertainment">entertainment</a></li>
-</div>
-<div className="search-icon" onClick={searchBtn}>
-<span className="fas fa-search"></span></div>
-<div className="cancel-icon" onClick={cancelBtn}>
-<span className="fas fa-times"></span></div>
-<form action="#">
-        <input type="search" className="search-data" placeholder="Search" required></input>
-        <button type="submit" className="fas fa-search">search</button>
-      </form>
-</nav>
-
+  
+ <div>
+    <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
     
    
 
@@ -63,8 +29,8 @@ NewsDesk</div>
     </div>
     
     
-
   )
+  
 
   
 }
