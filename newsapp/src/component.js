9f83ff55788ba4a0fa249ec701d5b4ee8df8afc2
invisiblegeek1 <48353856+invisiblegeek1@users.noreply.github.com>
2020-09-30 18:41:55 +0000

    readmore button bug fixed

--- newsapp/src/component.js
+++ newsapp/src/component.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React,{useState,useEffect} from "react";
 import "./component.css";
 import { withRouter } from "react-router-dom";
 import Data from './data';
-import { Button } from "react-bootstrap";
+
+
 
 function Card(props) {
   const [data, dataHandler] = useState(Data);
-  // useEffect(() => {
-  //   async function loadData() {
-  //     let url = props.match.params.id
-  //       ? `search/${props.match.params.id}`
-  //       : props.title;
-  //     fetch(`https://newszapp.herokuapp.com/${url}`)
-  //       .then((response) => response.json())
-  //       .then((res) => {
-  //         dataHandler(res.articles);
-  //       })
-  //       .catch((error) => console.log(error));
-  //   }
-  //   loadData();
-  // }, [props.title, props.match.params.id]);
+  useEffect(() => {
+    async function loadData() {
+      let url = props.match.params.id
+        ? `search/${props.match.params.id}`
+        : props.title;
+      fetch(`https://newszapp.herokuapp.com/${url}`)
+        .then((response) => response.json())
+        .then((res) => {
+          dataHandler(res.articles);
+        })
+        .catch((error) => console.log(error));
+    }
+    loadData();
+  }, [props.title, props.match.params.id]);
 
   const readmore = (index, res) => {
     props.history.push({
@@ -35,6 +36,9 @@ function Card(props) {
       {data.map((res, index) => {
         return (
           <div className="cardContainer" id="card" key={index}>
+            <p className="title">
+                {res.title.replace(/^(.{30}[^\s]*).*/, "$1") + "..."}
+              </p>
             <div className="imageContainer" id="photocontainer">
               <img
                 className="image"
@@ -48,18 +52,16 @@ function Card(props) {
               />
             </div>
             <div className="title-container">
-              <p className="title">
-                {res.title.replace(/^(.{50}[^\s]*).*/, "$1") + "..."}
-              </p>
               
-              <Button
+              
+              <button
                 className="readmorebtn"
                 variant="outline-primary"
                 size="lg"
                 onClick={() => readmore(index, res)}
               >
                 Readmore
-              </Button>
+              </button>
               
             </div>
           </div>
--- newsapp/src/readmore.js
+++ newsapp/src/readmore.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import "./readmore.css";
-import {Carousel} from 'react-bootstrap';
+
 
 const Readmore = (props) => {
   const input = props.location.data;
   const [inx, inHandler] = useState(props.location.index);
-  console.log(input[inx].content.split("[+")[0]);
+  
 
   return (
     <div className="readmore-container" key={inx}>
-      {/* <div className="readmoreimageContainer">
+      <div className="readmoreimageContainer">
         <img
           className="image"
           src={
@@ -25,27 +25,11 @@ const Readmore = (props) => {
       <h1 id="heading">{input[inx].title}</h1>
       <hr></hr>
 
-      <div className="content">{input[inx].content.split("[+")[0]}</div> */}
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 image"
-      src={
-        input[inx].urlToImage
-          ? input[inx].urlToImage
-          : "https://newsapi.org/images/n-logo-border.png"
-      }
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>{input[inx].title}</h3>
-      <p>{input[inx].content.split("[+")[0]}.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item></Carousel.Item>
-  </Carousel>
-      <button onClick={() => inHandler(inx - 1)}><span ><svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg></span></button>
-      <button onClick={() => inHandler(inx + 1)}><span ><svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></span></button>
+      <div className="content">{input[inx].description.split("[+")[0]}</div>
+      
+  
+        <button className="button1" onClick={() => inHandler(inx - 1)}>{"<"}</button>
+        <button className="button2" onClick={() => inHandler(inx + 1)}>{">"}</button>
     </div>
   );
 };
